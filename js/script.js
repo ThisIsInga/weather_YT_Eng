42bd1fc3ed5c3cb2410b79248199f7d0f9be25d0
ThisIsInga <155862681+ThisIsInga@users.noreply.github.com>
2024-10-24 11:19:41 +1000

    weather_YT_Eng

---
+++ js/script.js
@@ -0,0 +1,26 @@
+const apiKey = '8bd155d1c8177e3437c4765f690cc4ed'
+const city = 'Khabarovsk'
+
+async function getWeather() {
+    try{
+        const response = await fetch (`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`)
+        const data = await response.json();
+
+        if (response.status === 200){
+            document.getElementById('weather').innerHTML = `Temperature ${data.main.temp}C, ${data.weather[0].description}`;
+        } else {
+            document.getElementById('weather').innerHTML = `Error` + data.message;
+        }
+    } catch (error) {
+        document.getElementById('weather').innerHTML = `Error` + error.message;
+    }
+}
+
+function UpdateTime() {
+    const now = new Data();
+    const options = { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false};
+    document.getElementById('time').innerHTML = now.toLocaleDateString([], options);
+}
+
+setInterval(UpdateTime, 1000);
+getWeather();
